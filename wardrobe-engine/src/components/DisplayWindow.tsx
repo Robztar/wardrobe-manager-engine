462059c508d62943095aa9e438f481cd5a4fd4e0
Robztar <71354535+Robztar@users.noreply.github.com>
2024-02-05 23:03:29 -0500

    Setup for the Wardrobe Manager Web App
    Setup:
    - React/TS project built w/ Vite
    - Preliminary folder structure set up
    - Place-holder code put in

---
+++ wardrobe-engine/src/components/DisplayWindow.tsx
@@ -0,0 +1,10 @@
+
+
+// export const DisplayWindow = ({ aProp }: { aProp: React.FC }) =>{
+export const DisplayWindow = ({ aProp }: { aProp: number }) =>{
+     const bgDark = aProp;
+
+     return(
+          <div></div>
+     )
+}
